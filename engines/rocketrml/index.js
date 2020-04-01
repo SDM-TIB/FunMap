e80d595dbad81824639d9396c51f00689149bdd0
dachafra <dachafra@gmail.com>
2020-04-01 16:11:59 +0200

    script for rmlmapper

--- engines/rocketrml/index.js
+++ engines/rocketrml/index.js
@@ -3,7 +3,7 @@ var fs = require('fs')
 
 const doMapping = async () => {
   try{
-    aux = fs.readFileSync('./data.csv', 'utf8')
+    aux = fs.readFileSync('data.csv', 'utf8')
     mapping = fs.readFileSync('mapping.rml.ttl', 'utf8')
   }catch(e){
      console.log('Error:', e.stack);
@@ -16,20 +16,6 @@ const doMapping = async () => {
     verbose: true,
     xmlPerformanceMode: false,
     replace: false,
-    functions: {
-      'http://users.ugent.be/~bjdmeest/function/grel.ttl#toUpperCase': function (data) {
-                let result=data[0].toUpperCase(); 
-                return result;
-      },
-      'http://users.ugent.be/~bjdmeest/function/grel.ttl#string_replace': function (data) {
-                let result=data[0].replace(data[1],data[2]); 
-                return result;
-      },
-      'http://users.ugent.be/~bjdmeest/function/grel.ttl#toLowerCase': function (data) {
-                let result=data[0].toLowerCase(); 
-                return result;
-      }
-    }
   };
   const result = await parser.parseFileLive(mapping, inputFiles, options).catch((err) => { console.log(err); });
 };

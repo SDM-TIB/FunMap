00c18c2f321d22c894e21ce6fa260744add3c8c6
Sam J <samaneh.jozashoori@gmail.com>
2020-04-07 19:43:03 +0200

    Update functions.py
    empty value bug is fixed?!

--- engines/FunMap/translator/functions.py
+++ engines/FunMap/translator/functions.py
@@ -49,7 +49,7 @@ def match(value, regex):
 
 def variantIdentifier(column1, column2,prefix):
     value = ""
-    if (str(column1) != "" and "?" not in str(column1)):
+    if (str(column1) != "" and str(column1) != " "):
         value = re.sub('_.*','',str(column2))+"_"+str(column1).split(".")[1].replace(">", "~")
         value = prefix+value
     return value

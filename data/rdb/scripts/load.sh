b0e49a0a29aaa8b88a9b3a9d14c8aeaf9967a0c6
samiscoding <samaneh.jozashoori@gmail.com>
2020-08-14 11:58:42 +0200

    largeData db creation script is added!
    largeData db creation script is added!

--- data/rdb/scripts/load.sh
+++ data/rdb/scripts/load.sh
@@ -1,2 +1,3 @@
 mysql --local-infile=1 -u root -ptib -e 'source ../data/funmap-script.sql'
 mysql --local-infile=1 -u root -ptib -e 'source ../data/rdfizer-script.sql'
+mysql --local-infile=1 -u root -ptib -e 'source ../data/largeData-script.sql'

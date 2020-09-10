f6e4bd7cc3feec0f3cf4956d9bdd620a575360ab
dachafra <dachafra@gmail.com>
2020-09-10 13:48:41 +0200

    configurations and scripts for rdb

--- RMLMapper/run-rdb-v2.sh
+++ RMLMapper/run-rdb-v2.sh
@@ -1,43 +1,36 @@
 # /bin/bash
 
 ############################# Running over 20k-records dataset ##############################
+echo "---------Experiments over RDB with RMLMapper---------"
 
 ## mappings to be applied in the experiments
 cp ./mappings/complex_sameFunction10_RDB.ttl mapping.ttl
 
 ## create result files
-echo "dataset,mapping,time,iteration" > results-rdb-FunMap-rmlmapper.csv
+echo "dataset,mapping,time,iteration" > results-rdb-funmap-rmlmapper.csv
 echo "dataset,mapping,time,iteration" > results-rdb-rmlmapper.csv
 
-########################################################################################
-#################### Running FunMap+RMLMapper experiments ############################
-########################################################################################
-echo "---------Running FunMap with RMLMapper over RDB and complex functions---------"
+
+echo "Running experiment complex_sameFunction10_RDB.ttl"
 
 for i in 1 2 3 4 5
 do
-	echo "---------Running iteration $i---------"
+	echo "FunMap+RMLMapper over RDB: iteration $i"
 	start=$(date +%s.%N)
 	## Running FunMap:
 	python3 ./FunMap/run_translator.py config_rdb.ini
 	## Running RMLMapper over the results:
 	java -jar rmlmapper.jar -m mappings/transfered_mapping_joinInQuery_rmlmapper.ttl -o output.nt
 	dur=$(echo "$(date +%s.%N) - $start" | bc)
-	echo "veracity75.csv,complex_sameFunction10_RDB.ttl,$dur,$i"  >> results-rdb-FunMap-rmlmapper.csv
-done
+	echo "veracity75,complex_sameFunction10_RDB.ttl,$dur,$i"  >> results-rdb-funmap-rmlmapper.csv
 
-
-########################################################################################
-#################### Running RMLMapper+Functions experiments #########################
-########################################################################################
-echo "---------Running RMLMapper over RDB and complex functions---------"
-
-for i in 1 2 3 4 5
-do
-	echo "---------Running iteration $i---------"
+	echo "RMLMapper over RDB: iteration $i"
 	start=$(date +%s.%N)
 	java -jar rmlmapper.jar -m mapping.ttl -f functions_complex.ttl -o output-rdb.nt -d
 	dur=$(echo "$(date +%s.%N) - $start" | bc)
 	echo "veracity75.csv,complex_sameFunction10_RDB.ttl,$dur,$i"  >> results-rdb-rmlmapper.csv
 done
+
+
+rm mapping.ttl
 
--- SDM-RDFizer/run-rdbv2.sh
+++ SDM-RDFizer/run-rdbv2.sh
@@ -1,13 +1,13 @@
 # /bin/bash
 
 ############################# Running arrays ##############################
-echo "---------Experiments over RDB with SDM-RDFizer (note that experiments with complex_sameFunction10_RDB_5millionRecords.ttl could not finish in SDM-RDFizer basic configuration---------"
+echo "---------Experiments over RDB with SDM-RDFizer (note that experiments with complex_sameFunction10_RDB_5millionRecords.ttl could not finish in SDM-RDFizer configuration---------"
 
 declare -a mappings=("complex_sameFunction10_RDB.ttl" "complex_sameFunction10_RDB_5millionRecords.ttl")
 
 ## create result files
-echo "mapping,time,iteration" > results-rdb-FunMap-SDM.csv
-echo "mapping,time,iteration" > results-rdb-SDM.csv
+echo "mapping,time,iteration" > results-rdb-funmap-sdmrdfizer.csv
+echo "mapping,time,iteration" > results-rdb-sdmrdfizer.csv
 
 ########################################################################################
 #################### Running FunMap+SDM-RDFizer experiments ############################
@@ -16,18 +16,19 @@ echo "---------Running FunMap with SDM-RDFizer---------"
 for mapping in "${mappings[@]}"
 do
 	cp ./mappings/$mapping mapping.ttl
+	echo "Running experiment $mapping"
 	for i in 1 2 3 4 5
 	do
-		echo "---------Running mapping $mapping iteration---------"
+		echo "FunMap+SDM-RDFizer over RDB: iteration $i"
 		start=$(date +%s.%N)
 		## Running FunMap:
 		python3 ./FunMap/run_translator.py config-rdb-funmap.ini
 		## Running SDM-RDFizer over the results:
 		python3 ./SDM-RDFizer/rdfizer/run_rdfizer.py config-rdb-SDM.ini
 		dur=$(echo "$(date +%s.%N) - $start" | bc)
-		echo "$mapping,$dur,$i"  >> results-rdb-FunMap-SDM.csv
-		sed -i 's/db: funmap/db: largeData/g' config-rdb-funmap.ini	
+		echo "$mapping,$dur,$i"  >> results-rdb-funmap-sdmrdfizer.csv
 	done
+	sed -i 's/db: funmap/db: largeData/g' config-rdb-funmap.ini	
 done
 
 sed -i 's/db: largeData/db: funmap/g' config-rdb-funmap.ini	
@@ -36,17 +37,18 @@ sed -i 's/db: largeData/db: funmap/g' config-rdb-funmap.ini
 ########################################################################################
 #################### Running SDM-RDFizer+Functions experiments #########################
 ########################################################################################
-echo "---------Running FunMap with SDM-RDFizer---------"
+echo "---------Running SDM-RDFizer---------"
 for mapping in "${mappings[@]}"
 do
 	cp ./mappings/$mapping mapping.ttl
+	echo "Running experiment $mapping"
 	for i in 1 2 3 4 5
 	do
-		echo "---------Running mapping $mapping iteration $i---------"
+		echo "SDM-RDFizer over RDB: itearion $i"
 		start=$(date +%s.%N)
 		python3 ./SDM-RDFizer-Functions/rdfizer/run_rdfizer.py config-rdb-SDM-function.ini
 		dur=$(echo "$(date +%s.%N) - $start" | bc)
-		echo "$mapping,$dur,$i"  >> results-rdb-SDM.csv
+		echo "$mapping,$dur,$i"  >> results-rdb-sdmrdfizer.csv
 		sed -i 's/db: funmap/db: largeData/g' config-rdb-SDM-function.ini
 	done
 done

8b36726f6f6bf29be27ebcaabbe25c68d47896dd
dachafra <dachafra@gmail.com>
2020-09-10 17:26:25 +0200

    enforce python3

--- SDM-RDFizer/preparation.sh
+++ SDM-RDFizer/preparation.sh
@@ -2,12 +2,12 @@
 
 echo "Installing FunMap..."
 cd FunMap/
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Installing SDM-RDFizer without FnO support..."
 cd ../SDM-RDFizer/
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Installing SDM-RDFizer with FnO support..."
 cd ../SDM-RDFizer-Functions/
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
